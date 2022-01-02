57c4f7c5bf4da8d143360ce99afe3fad89a0e832
devil-cyber <mani2474695@gmail.com>
2022-01-02 13:53:23 +0530

    Updates

---
+++ precache-manifest.65d916b5532601232e6c94f59f6c2229.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741c2709a5d25595fc0941c4e5698acf",
+    "url": "/manikant/index.html"
+  },
+  {
+    "revision": "ae6ab34845c2320d75d3",
+    "url": "/manikant/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "3100ec999dd2ed8ed275",
+    "url": "/manikant/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "09d886334ee7f15b3056",
+    "url": "/manikant/static/css/main.ee1471ad.chunk.css"
+  },
+  {
+    "revision": "012b17d854491bceb2c9",
+    "url": "/manikant/static/js/2.4993cc89.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/manikant/static/js/2.4993cc89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6ab34845c2320d75d3",
+    "url": "/manikant/static/js/3.99ea70e5.chunk.js"
+  },
+  {
+    "revision": "3100ec999dd2ed8ed275",
+    "url": "/manikant/static/js/4.ebcc580d.chunk.js"
+  },
+  {
+    "revision": "09d886334ee7f15b3056",
+    "url": "/manikant/static/js/main.0ec02e4a.chunk.js"
+  },
+  {
+    "revision": "13d99ade97bd9d9b6b8e",
+    "url": "/manikant/static/js/runtime-main.9a7b272f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/manikant/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/manikant/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d8c9be0daec0861d39db1ab7cd21e7fb",
+    "url": "/manikant/static/media/bh.d8c9be0d.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/manikant/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/manikant/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "513abada7c7a253f7369fa564fb32841",
+    "url": "/manikant/static/media/editrox.513abada.png"
+  },
+  {
+    "revision": "d38a06be295aab3c83c2a9611eb17c6c",
+    "url": "/manikant/static/media/hack.d38a06be.png"
+  },
+  {
+    "revision": "df197cadf385ac9c34ebbe7ad52ad9fa",
+    "url": "/manikant/static/media/ine.df197cad.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/manikant/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "536fb3844ba0616a043dc455ed100c86",
+    "url": "/manikant/static/media/mlogo.536fb384.png"
+  },
+  {
+    "revision": "dd32705c1878efda194a36370105a1e3",
+    "url": "/manikant/static/media/nitap.dd32705c.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/manikant/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/manikant/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "bde8ff6e38eca2230d5e3ce190fb7c11",
+    "url": "/manikant/static/media/ud.bde8ff6e.png"
+  }
+]);
